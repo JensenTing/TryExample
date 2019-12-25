dcbeed283bdd24c994689fb3c233f9a0281d6001
JensenTing <49519172+JensenTing@users.noreply.github.com>
2019-12-25 10:59:09 +0800

    Update PIDControl.py

--- PIDControl.py
+++ PIDControl.py
@@ -111,14 +111,13 @@ def run(robot, tau_p, tau_d, tau_i, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
     prev_cte = robot.y
-    sum_cte = 0
+    int_cte = 0
     for i in range(n):
         cte = robot.y
-        sum_cte += cte
-        steer = - tau_p * cte \
-                - tau_d * (cte - prev_cte) \
-                - tau_i * sum_cte
+        diff_cte = cte - prev_cte
         prev_cte = cte
+        int_cte += cte
+        steer = -tau_p * cte - tau_d * diff_cte - tau_i * int_cte
         robot.move(steer, speed)
         x_trajectory.append(robot.x)
         y_trajectory.append(robot.y)
